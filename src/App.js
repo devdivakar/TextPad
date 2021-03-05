0345f40a8abc0253fb1bafb3b4403bc74707ed34
Karthik K B <karthik.consultant@embibe.com>
2021-03-05 15:42:04 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ const App =()=> {
 
   return (
     <div className="App">
-      <textarea className= "textbox" placeholder= "Write your text"   name="text" onChange = {(e)=>{setText(e.currentTarget.value)}} id="text1" style={{width: '100%', minHeight: '100vh',fontWeight:'400',pointer:'text', fontSize : '20px', color : 'rgb(179 243 255)', backgroundColor: '#002b36',border:'none',height:'calc(100% - 22px)'}}  spellcheck="true"></textarea>
+      <textarea className= "textbox" placeholder= "// Write your text here because simple is the best"   name="text" onChange = {(e)=>{setText(e.currentTarget.value)}} id="text1" style={{width: '100%', minHeight: '100vh',fontWeight:'400',pointer:'text', fontSize : '20px', color : 'rgb(179 243 255)', backgroundColor: '#002b36',border:'none',height:'calc(100% - 22px)'}}  spellcheck="true"></textarea>
       <button style = {{background:'#5b5bd3', outline:'none', fontWeight:'bold', padding: '0px', color : '#fff',borderRadius:'5px',border:'none',padding:'10px',position:'Fixed',left:'10px',bottom:'10px',cursor:'pointer'}} onClick = {()=>saveTextAsFile(text,'download.txt')}><span className="btn-text">Download</span><img className="download" src={download}/></button>
     </div>
   );
