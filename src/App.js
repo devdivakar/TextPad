72d6949f54e10ded4d6848062af24bd94b045c34
Karthik K B <karthik.consultant@embibe.com>
2021-03-05 13:59:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import download from '../src/assets/download.png';
+const destroyClickedElement=(event)=>{
+  document.body.removeChild(event.target);
+}
+const saveTextAsFile = (textToWrite, fileNameToSaveAs)=> {
+      let textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+      let downloadLink = document.createElement("a");
+      downloadLink.download = fileNameToSaveAs;
+      downloadLink.innerHTML = "Download File";
+      if (window.webkitURL != null) {
+        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+      }
+      else {
+        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        downloadLink.onclick = destroyClickedElement;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+      }
+      
+      downloadLink.click();
+}
+const App =()=> {
+  const [text, setText] = useState('');
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea className= "textbox"   name="text" onChange = {(e)=>{setText(e.currentTarget.value)}} id="text1" style={{width: '100%', minHeight: '100vh',fontWeight:'400',pointer:'text', fontSize : '20px', color : 'rgb(179 243 255)', backgroundColor: '#002b36',border:'none',height:'calc(100% - 22px)'}}  spellcheck="true"></textarea>
+      <button style = {{background:'#5b5bd3', outline:'none', fontWeight:'bold', padding: '0px', color : '#fff',borderRadius:'5px',border:'none',padding:'10px',position:'Fixed',left:'10px',bottom:'10px',cursor:'pointer'}} onClick = {()=>saveTextAsFile(text,'download.txt')}><span className="btn-text">Download</span><img className="download" src={download}/></button>
     </div>
   );
 }
